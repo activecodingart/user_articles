91f1a6beb282790df9c6aa26fa01b6c0505de1a6
activecodingart <piotrbrzezicki81@gmail.com>
2023-07-28 17:51:53 +0200

    Add injectable

--- lib/data/remote_data_sources/articles_remote_data_source.dart
+++ lib/data/remote_data_sources/articles_remote_data_source.dart
@@ -1,24 +1,16 @@
 import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:user_articles/domain/models/article_model.dart';
 
 part 'articles_remote_data_source.g.dart';
 
-@RestApi(baseUrl: "https://my-json-server.typicode.com/adamsmaka/json-demo")
+@injectable
+@RestApi()
 abstract class ArticlesRemoteRetrofitDataSource {
-  factory ArticlesRemoteRetrofitDataSource(Dio dio, {String baseUrl}) = _ArticlesRemoteRetrofitDataSource;
+  @factoryMethod
+  factory ArticlesRemoteRetrofitDataSource(Dio dio) = _ArticlesRemoteRetrofitDataSource;
 
   @GET("/articles")
   Future<List<ArticleModel>> getArticles();
 }
-
-// class ArticlesRemoteDioDataSource {
-//   Future<List<Map<String, dynamic>>?> getArticles() async {
-//     final response = await Dio().get<List<dynamic>>('https://my-json-server.typicode.com/adamsmaka/json-demo/articles');
-//     final listDynamic = response.data;
-//     if (listDynamic == null) {
-//       return null;
-//     }
-//     return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-//   }
-// }
